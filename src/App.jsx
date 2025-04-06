90b2e1e89ba1d58b65dd3de1f7b17280a8d361b4
Yessica Belén Reynoso <reynosoyessica92@gmail.com>
2025-04-06 11:52:22 -0300

    Paginación funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import AddProduct from './pages/AddProduct';
+import ProductDetail from './pages/ProductDetail';
 import React, { useEffect, useState } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Container, Typography, Grid, Pagination, Box } from '@mui/material';
-
+import Navbar from './components/Navbar';
 
 
 
@@ -17,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/add-product" element={<AddProduct />} />
           <Route path="/product/:id" element={<ProductDetail />} />
+          
         </Routes>
       </Container>
     </>
