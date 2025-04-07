4caf88ff3452c2adfe4e688922d315d30b94d43d
Yessica Belén Reynoso <reynosoyessica92@gmail.com>
2025-04-06 22:03:19 -0300

    corrijo NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,47 @@
-<AppBar
-  position="static"
-  elevation={0}
-  sx={{
-    mb: 4,
-    bgcolor: '#FFE4E1',
-    color: '#4A4A4A',
-    fontFamily: 'Poppins, sans-serif',
-    height: 64 
-  }}
->
-  <Toolbar sx={{ minHeight: 64, px: 2 }}> 
-    <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
-      <img
-        src="/assets/logo.png"
-        alt="Logo GestorRuff"
-        style={{ width: 90, height: 90, marginRight: 10 }}
-      />
-    </Box>
-    <Button
-      component={RouterLink}
-      to="/"
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
       sx={{
-        backgroundColor: '#FFB6B9',
-        color: '#fff',
-        fontWeight: 'bold',
-        textTransform: 'none',
-        '&:hover': {
-          backgroundColor: '#FF9AA2',
-        },
+        mb: 4,
+        bgcolor: '#FFE4E1',
+        color: '#4A4A4A',
+        fontFamily: 'Poppins, sans-serif',
+        height: 64 // Altura del navbar
       }}
     >
-      Inicio
-    </Button>
-  </Toolbar>
-</AppBar>
+      <Toolbar sx={{ minHeight: 64, px: 2 }}>
+        <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
+          <img
+            src="src\assets\logo.png"
+            alt="Logo GestorRuff"
+            style={{ width: 90, height: 90, marginRight: 10 }}
+          />
+        </Box>
+
+        <Button
+          component={RouterLink}
+          to="/"
+          sx={{
+            backgroundColor: '#FFB6B9',
+            color: '#fff',
+            fontWeight: 'bold',
+            textTransform: 'none',
+            '&:hover': {
+              backgroundColor: '#FF9AA2',
+            },
+          }}
+        >
+          Inicio
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
